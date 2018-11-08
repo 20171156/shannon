f42beb2c8208ebd0dcdf6b9c0c1593d046b002f6
shannon <20171156@game.hoseo.edu>
2018-11-08 18:21:12 +0900

    PP07.TextureManager

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -1,59 +1,43 @@
 #include <iostream>
-#include "Game.h"
+#include <SDL.h>
 #include <SDL_image.h>
+#include "Game.h"
+using namespace std;
 
-bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+bool Game::init(const char* title, int xpos, int ypos,
+	int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
 
-		//SDL_Surface* pTempSurface = IMG_Load("Assets/animate.png");
-		SDL_Surface* pTempSurface = IMG_Load("Assets/animate-alpha.png");
-		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-		SDL_FreeSurface(pTempSurface);
+		m_bRunning = true;
+
+		m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
 		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
-
-
-		m_sourceRectangle.w = 128;
-		m_sourceRectangle.h = 82;
-		m_destinationRectangle.x = m_sourceRectangle.x = 0;
-		m_destinationRectangle.y = m_sourceRectangle.y = 0;
-		m_destinationRectangle.w = m_sourceRectangle.w;
-		m_destinationRectangle.h = m_sourceRectangle.h;
-
-		//SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-		//2번째:포맷
-		//3번째 : 접근
-		//4/5번째 : 가로, 세로 크기..image의 크기를 저장한다
 	}
-
 	else
 	{
-		return false;
+		return false; // sdl could not initialize
 	}
-	m_bRunning = true;
 	return true;
 }
 
 void Game::render()
 {
-
-
-
 	SDL_RenderClear(m_pRenderer);
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
+	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
 	SDL_RenderPresent(m_pRenderer);
-
 }
 
 void Game::clean()
 {
-	std::cout << "cleaning game\n";
+	cout << "cleaning game\n";
 	SDL_DestroyWindow(m_pWindow);
 	SDL_DestroyRenderer(m_pRenderer);
 	SDL_Quit();
@@ -77,5 +61,5 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
